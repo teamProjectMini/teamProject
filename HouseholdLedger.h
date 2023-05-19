e2de1803484bdd120228c51c90729a87dcc621de
apple0605a <apple0605a@gmail.com>
2023-05-19 21:30:54 +0900

    헤더파일 내용 임시 작성

---
+++ HouseholdLedger.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Structure
+typedef struct {
+	char name[50];	//항목명
+	int in_or_out;	//수입:  1, 지출: 0	수입, 지출 예정 여부도 포함?
+	int price; 	//수입 및 지출 금액
+	int type;	//분야, 항목	각 번호 정할 필요 有
+} item;
+
+//CRUD
+//create
+
+//read
+
+//update
+
+//delete
+
+
