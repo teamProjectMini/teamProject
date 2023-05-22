6e0f003e2f4584c405a678f2a2384dad3d48fc62
rkdwndud2003 <rkdwndud2003@gmail.com>
2023-05-22 12:10:32 +0900

    헤더파일 강주영 파트 변경

--- HouseholdLedger.h
+++ HouseholdLedger.h
@@ -10,13 +10,30 @@ typedef struct {
 	int type;	//분야, 항목	각 번호 정할 필요 有
 } item;
 
+//기본기능
+void print_head();
+
 //CRUD
 //create
-void add_item(item *t);
+int add_item(item *t);
 //read
-void read_item(item t);
+void read_item(item *t);
 //update
-void update_item(item *t);
+int update_item(item *t);
 //delete
-void delete_item(item *t);
+int delete_item(item *t[], int count);
 
+//리스트 전체 출력
+void list_item(item *t[], int count);
+
+//가계부 기능
+//분야별 지출 내역 조회
+void read_item_type(item t);
+//총 지출 조회
+
+//사용자와 또래지출 비교
+
+//지출 예정 기록
+
+
+//FILE 저장 및 수정
